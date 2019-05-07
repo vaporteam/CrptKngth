84047514310b462c3a6cd68890dc67149c29c967
Pablo Cruz Mendez <pablo.cruz@metrica.es>
2019-05-07 16:04:14 +0200

    m'men

---
+++ Crptonight/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crptonight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ♭
+            Console.WriteLine("");
+            Console.ReadKey(false);
+        }
+    }
+}
